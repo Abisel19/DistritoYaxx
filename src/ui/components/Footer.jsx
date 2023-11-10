ffbf70710c9c36ad339c40916d7309e9debd85bc
Brian Rios <briosm0601@gmail.com>
2023-11-10 04:20:34 -0500

    footer terminado

--- src/ui/components/Footer.jsx
+++ src/ui/components/Footer.jsx
@@ -2,72 +2,67 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <div className='bg-black py-20'>
-        <div className='container mx-auto px-0 xl:px-0 2xl:px-10'>
-          <div className='flex justify-center pb-10'>
-            <button className='w-[8.75rem]'>
-              <img src="/img/Logo.png" alt="logo" />
-            </button>
-          </div>
+    <footer className='bg-black'>
+      <div className="flex justify-center p-8 md:pt-12 xl:pt-12">
+        <a href="#"><img className="md:hidden" src="/img/Logo s.png" alt="" /><img className="hidden sm:block" src="/img/Logo m.png" alt="" /></a>
+      </div>
 
-          <div className='text-white flex justify-center pb-10'>
-            <button className='w-1/5 text-[30px] primary'>INICIO</button>
-            <button className='w-1/5 text-[30px] primary'>QUIENES SOMOS</button>
-            <button className='w-1/5 text-[30px] primary'>PROYECTO</button>
-            <button className='w-1/5 text-[30px] primary'>AMENIDADES</button>
-            <button className='w-1/5 text-[30px] primary'>CONTACTO</button>
-          </div>
+      <div className="flex items-center flex-col justify-center md:flex-row md:space-x-16">
+        <a className="text-white text-[14px] md:text-[30px] pt-1" href="#">INICIO</a>
+        <a className="text-white text-[14px] md:text-[30px] pt-1" href="#">QUIENES SOMOS</a>
+        <a className="text-white text-[14px] md:text-[30px] pt-1" href="#">PROYECTO</a>
+        <a className="text-white text-[14px] md:text-[30px] pt-1" href="#">AMENIDADES</a>
+        <a className="text-white text-[14px] md:text-[30px] pt-1" href="#">CONTACTO</a>
+      </div>
 
-          <div className='text-white flex justify-center pb-10'>
-            <svg className='mr-2.5 mt-2' xmlns="http://www.w3.org/2000/svg" width="19.442" height="21.689" viewBox="0 0 19.442 21.689">
-                <g id="Group_262" data-name="Group 262" transform="translate(3198.273 304.39)">
-                  <path id="Path_429" data-name="Path 429" d="M-3198.273-299.094a7.153,7.153,0,0,1,1.632-4.4,2.026,2.026,0,0,1,1.209-.677,10.105,10.105,0,0,1,1.331-.216.57.57,0,0,1,.407.22,3.5,3.5,0,0,1,.37.627c.627,1.2,1.243,2.412,1.877,3.609.348.656.351.96-.141,1.5-.383.421-.812.79-1.217,1.185-.176.171-.353.342-.517.527a.974.974,0,0,0-.16,1.385c.289.57.587,1.136.9,1.69a14.192,14.192,0,0,0,4.814,5.072c.356.225.713.45,1.075.663a.6.6,0,0,0,.834-.165c.339-.408.658-.836.989-1.252.235-.295.461-.6.719-.871.483-.5.72-.507,1.289-.13.961.636,1.926,1.263,2.891,1.892.27.177.545.344.814.524a.723.723,0,0,1,.322.7,6.084,6.084,0,0,1-.461,2.134,2.679,2.679,0,0,1-.69.88,5.307,5.307,0,0,1-3.759,1.493,6.491,6.491,0,0,1-2.2-.573,19.637,19.637,0,0,1-3.175-1.485,20.169,20.169,0,0,1-4.109-3.643,20.281,20.281,0,0,1-3.162-4.472,22.97,22.97,0,0,1-1.789-5.272A7.506,7.506,0,0,1-3198.273-299.094Z" fill="#fff"/>
-                </g>
-            </svg>
-            <a className='text-[28px] primary' href="#">informes@distritoyaax.com</a>
-            <p className='text-[28px] primary px-5'>|</p>
-            <a className='text-[28px] primary' href="#">+52 998 8705400</a>
-          </div>
+      <div className="md:hidden flex items-center justify-center space-x-8 pt-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="10.303" height="19.235" viewBox="0 0 10.303 19.235">
+          <path id="Path_547" data-name="Path 547" d="M9.627,10.851l.534-3.481H6.822V5.11A1.74,1.74,0,0,1,8.783,3.23H10.3V.266a18.508,18.508,0,0,0-2.7-.235C4.855.031,3.058,1.7,3.058,4.716V7.37H0v3.481H3.058v8.415H6.822V10.851Z" transform="translate(0 -0.031)" fill="#fff"/>
+        </svg>
+            
+        <svg xmlns="http://www.w3.org/2000/svg" width="19.235" height="19.236" viewBox="0 0 19.235 19.236">
+          <g id="Group_264" data-name="Group 264" transform="translate(-202.445 -4951.921)">
+            <path id="Path_544" data-name="Path 544" d="M96.327,6.348a1.128,1.128,0,1,0,1.128,1.128,1.128,1.128,0,0,0-1.128-1.128" transform="translate(120.828 4949.009)" fill="#fff"/>
+            <path id="Path_545" data-name="Path 545" d="M84.074,9.008a4.737,4.737,0,1,0,4.737,4.737,4.743,4.743,0,0,0-4.737-4.737m0,7.772a3.034,3.034,0,1,1,3.034-3.034,3.038,3.038,0,0,1-3.034,3.034" transform="translate(128.066 4947.795)" fill="#fff"/>
+            <path id="Path_546" data-name="Path 546" d="M83.673,19.266h-7.68a5.784,5.784,0,0,1-5.777-5.778V5.808A5.784,5.784,0,0,1,75.993.03h7.68a5.785,5.785,0,0,1,5.778,5.778v7.68a5.785,5.785,0,0,1-5.778,5.778M75.993,1.84a3.972,3.972,0,0,0-3.968,3.968v7.68a3.972,3.972,0,0,0,3.968,3.968h7.68a3.972,3.972,0,0,0,3.968-3.968V5.808A3.972,3.972,0,0,0,83.673,1.84Z" transform="translate(132.229 4951.892)" fill="#fff"/>
+          </g>
+        </svg>
+            
+        <svg xmlns="http://www.w3.org/2000/svg" width="16.076" height="18.446" viewBox="0 0 16.076 18.446">
+          <g id="Group_244" data-name="Group 244" transform="translate(0)">
+            <path id="Path_458" data-name="Path 458" d="M-881.714,781.505a4.42,4.42,0,0,1-3.477-3.915v-.407h-3.177v12.608a2.671,2.671,0,0,1-2.68,2.661,2.671,2.671,0,0,1-2.116-1.054h0a2.67,2.67,0,0,1,.511-3.741,2.67,2.67,0,0,1,2.425-.418v-3.228A5.837,5.837,0,0,0-896.81,789a5.837,5.837,0,0,0,1.607,4.878,5.838,5.838,0,0,0,8.255.095,5.838,5.838,0,0,0,1.757-4.174v-6.443a7.545,7.545,0,0,0,4.4,1.407V781.6A4.46,4.46,0,0,1-881.714,781.505Z" transform="translate(896.865 -777.184)" fill="#fff"/>
+          </g>
+        </svg>
+      </div>
 
-          <div className='flex justify-center pb-10'>
-            <div className='pr-5 '>
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18.18" height="33.94" viewBox="0 0 14.344 26.781">
-                  <path id="Path_466" data-name="Path 466" d="M13.4,15.1l.744-4.847H9.5V7.1a2.423,2.423,0,0,1,2.731-2.618h2.116V.359A25.768,25.768,0,0,0,10.59.031c-3.83,0-6.332,2.321-6.332,6.523v3.695H0V15.1H4.258V26.812H9.5V15.1Z" transform="translate(0 -0.031)" fill="#fff"/>
-                </svg>
-              </button>
-            </div>
+      <div className="hidden sm:flex justify-center md:space-x-10 md:pt-12">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18.178" height="33.938" viewBox="0 0 18.178 33.938">
+          <path id="Path_136" data-name="Path 136" d="M16.986,19.121l.943-6.142H12.036V8.993c0-1.681.823-3.318,3.461-3.318h2.681V.446A32.655,32.655,0,0,0,13.42.031C8.567.031,5.4,2.972,5.4,8.3V12.98H0v6.142H5.4V33.969h6.641V19.121Z" transform="translate(0 -0.031)" fill="#fff"/>
+        </svg>
+            
+        <svg xmlns="http://www.w3.org/2000/svg" width="33.938" height="33.938" viewBox="0 0 33.938 33.938">
+          <g id="Group_265" data-name="Group 265" transform="translate(-944.353 -10501.219)">
+            <path id="Path_137" data-name="Path 137" d="M97.188,6.348a1.989,1.989,0,1,0,1.989,1.989,1.99,1.99,0,0,0-1.989-1.989" transform="translate(873.117 10500.931)" fill="#fff"/>
+            <path id="Path_138" data-name="Path 138" d="M87.7,9.008a8.358,8.358,0,1,0,8.358,8.359A8.368,8.368,0,0,0,87.7,9.008m0,13.712a5.354,5.354,0,1,1,5.353-5.353A5.36,5.36,0,0,1,87.7,22.72" transform="translate(873.765 10500.822)" fill="#fff"/>
+            <path id="Path_139" data-name="Path 139" d="M93.96,33.969H80.41A10.206,10.206,0,0,1,70.216,23.775V10.225A10.206,10.206,0,0,1,80.41.03H93.96a10.207,10.207,0,0,1,10.195,10.195v13.55A10.207,10.207,0,0,1,93.96,33.969M80.41,3.223a7.009,7.009,0,0,0-7,7v13.55a7.009,7.009,0,0,0,7,7H93.96a7.009,7.009,0,0,0,7-7V10.225a7.009,7.009,0,0,0-7-7Z" transform="translate(874.137 10501.188)" fill="#fff"/>
+          </g>
+        </svg>
+            
+        <svg xmlns="http://www.w3.org/2000/svg" width="28.363" height="32.547" viewBox="0 0 28.363 32.547">
+          <g id="Group_201" data-name="Group 201" transform="translate(896.865 -777.184)">
+            <path id="Path_458" data-name="Path 458" d="M-870.134,784.809a7.8,7.8,0,0,1-6.134-6.907v-.718h-5.605V799.43a4.712,4.712,0,0,1-4.729,4.7,4.712,4.712,0,0,1-3.734-1.859l0,0,0,0a4.71,4.71,0,0,1,.9-6.6,4.711,4.711,0,0,1,4.278-.738v-5.695a10.3,10.3,0,0,0-11.611,8.791,10.3,10.3,0,0,0,2.834,8.606,10.3,10.3,0,0,0,14.565.167,10.3,10.3,0,0,0,3.1-7.365V788.064a13.313,13.313,0,0,0,7.766,2.483v-5.57A7.862,7.862,0,0,1-870.134,784.809Z" transform="translate(0 0)" fill="#fff"/>
+          </g>
+        </svg>
+      </div>
 
-            <div className='pr-5 pl-5'>
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="33.94" height="33.94" viewBox="0 0 26.781 26.781">
-                  <g id="Group_263" data-name="Group 263" transform="translate(-1829.117 -78.885)">
-                    <path id="Path_469" data-name="Path 469" d="M96.769,6.348a1.57,1.57,0,1,0,1.57,1.57,1.57,1.57,0,0,0-1.57-1.57" transform="translate(1752.828 77.319)" fill="#fff"/>
-                    <path id="Path_467" data-name="Path 467" d="M85.933,9.008a6.6,6.6,0,1,0,6.6,6.6,6.6,6.6,0,0,0-6.6-6.6m0,10.82A4.225,4.225,0,1,1,90.157,15.6a4.23,4.23,0,0,1-4.224,4.224" transform="translate(1756.685 76.672)" fill="#fff"/>
-                    <path id="Path_468" data-name="Path 468" d="M88.952,26.811H78.26a8.053,8.053,0,0,1-8.044-8.045V8.075A8.053,8.053,0,0,1,78.26.03H88.952A8.054,8.054,0,0,1,97,8.075V18.767a8.054,8.054,0,0,1-8.045,8.045M78.26,2.55a5.531,5.531,0,0,0-5.525,5.525V18.767a5.531,5.531,0,0,0,5.525,5.525H88.952a5.531,5.531,0,0,0,5.525-5.525V8.075A5.531,5.531,0,0,0,88.952,2.55Z" transform="translate(1758.901 78.855)" fill="#fff"/>
-                  </g>
-                </svg>
-              </button>
-            </div>
+      <div div className="flex flex-col items-center pt-4 md:flex-row md:justify-center">
+        <a className="text-white text-[13px] md:text-[28px] md:order-last" href="#">+52 998 8705400</a>
+        <a className="hidden sm:inline-flex text-white text-[28px] px-3" href="#">|</a>
+        <a className="text-white text-[13px] md:text-[28px] md:order-first" href="#">informes@distritoyaax.com</a>
+      </div>
 
-            <div className='pl-5'>
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="28.36" height="32.55" viewBox="0 0 22.382 25.683">
-                  <g id="Group_210" data-name="Group 210" transform="translate(0 0)">
-                    <path id="Path_458" data-name="Path 458" d="M-875.771,783.2a6.154,6.154,0,0,1-4.841-5.45v-.566h-4.423v17.554a3.718,3.718,0,0,1-3.731,3.705,3.718,3.718,0,0,1-2.946-1.467h0a3.717,3.717,0,0,1,.711-5.208,3.717,3.717,0,0,1,3.376-.582v-4.494a8.126,8.126,0,0,0-9.162,6.937,8.126,8.126,0,0,0,2.237,6.791,8.128,8.128,0,0,0,11.493.132,8.128,8.128,0,0,0,2.447-5.812v-8.97a10.5,10.5,0,0,0,6.128,1.959v-4.4A6.2,6.2,0,0,1-875.771,783.2Z" transform="translate(896.865 -777.184)" fill="#fff"/>
-                  </g>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div className='flex justify-center'>
-            <div className='text-white'>
-              <p className='text-[20px] primary'>COPYRIGHT 2023 | MADE BY BWE AGENCY</p>
-            </div>
-          </div>
-        </div>
+      <div className="flex items-center justify-center p-5 pb-9 md:p-10">
+        <h3 className="text-white text-[12px] md:text-[20px]">COPYRIGHT 2023 | MADE BY BWE AGENCY</h3>
       </div>
     </footer>
   );
